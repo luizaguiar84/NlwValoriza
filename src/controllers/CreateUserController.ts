12c324641ffbd9dba1154de57e3f93de1a206bee
Luiz <43261487+luizaguiar84@users.noreply.github.com>
2021-06-23 22:36:58 -0300

    Aula dia 4

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -3,11 +3,16 @@ import { CreateUserService } from "../services/CreateUserService";
 
 class CreateUSerController {
   async handle(request: Request, response: Response) {
-    const { name, email, admin } = request.body;
+    const { name, email, admin, password } = request.body;
     console.log("name", name);
     const createUserService = new CreateUserService();
 
-    const user = await createUserService.execute({ name, email, admin });
+    const user = await createUserService.execute({
+      name,
+      email,
+      admin,
+      password,
+    });
 
     return response.json(user);
   }
